590b58ac4d7c17e415255cb9d2ed2935c8172a1d
Khoo Yong Jun <khoo.jack@gmail.com>
2017-10-05 19:39:08 +0800

    some updates on countdown

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # visualpro
 An app use to display dynamic lyrics with graphical/video background which can use for countdown and viewing videos, build in electron.io and AngularJS.
+
+Approach
+# Countdown
+Receive input time, and start counting down at the rendere.
+Open rendere if it is not opened.
+
+#Song
+Maintain song using Angular and store it in application internally with electron for easier browsing.
+1. Artist - name, id, 
+2. Album - name, id
+3. Song - name, artist_id, album_id, id, lyric, isArchived
+4. Lyric - en, cn, st
+
+#Config
+Maintain the display style
+## Attributes
+1. Font-size
+2. Font-weight (bold)
+3. Font-style (italic, underline)
+4. Color
+5. Font
+6. Text-align 
+7. Verticle-align
+** All has a pair of value
+
+#Project
+Maintain, import songs, configuration
+Save Externally
+
+#Console
+Control the renderer
+
+#User Experience
+Optimization
+Documentation/Tutorial
+Loading cover
+
+#Import/Export
+Internal file
---
+++ assets/script/countdown.js
@@ -0,0 +1,67 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+require('./../../vendor/jquery.min.js')
+$(function(){  
+  var renderer
+  let countDown
+  let counting = function(){
+
+    if (interval >= 0) {
+      $(renderer.document).find('#countdown').fadeIn(2000);
+      countDown = setInterval(() => {
+          countdownSecond = interval % 60
+          countdownMinute = (interval % 3600 - countdownSecond) / 60
+          countdownHour = (interval - countdownMinute * 60 - countdownSecond) / 3600
+          text = ''
+          text += countdownHour ? countdownHour + ':' : ''
+          text += (countdownMinute < 10 ? (countdownHour? '0' : '') + countdownMinute : countdownMinute) + ':'
+          text += countdownSecond < 10 ? '0' + countdownSecond : countdownSecond
+          $(renderer.document).find('#countdown').html(text)
+          if (!(interval > 0)) {
+            stopCounting();
+            $(renderer.document).find('#countdown').fadeOut(2000)
+          }
+        console.log(interval--)
+      }, 1000);
+    } 
+  }
+  let stopCounting = function(){
+    clearInterval(countDown)
+  }
+  $('#stop').on('click', () => {
+    stopCounting()
+  })
+
+  $('#countdown').on('click', () => {
+    stopCounting()
+    renderer = window.open('./assets/view/renderer.html', 'renderer')
+    time = $('input').val()
+    endHour = time[0] + time[1]
+    endMinute = time[3] + time[4]
+    endSecond = '00'
+
+    now = new Date();
+    nowHour = now.getHours()
+    nowMinute = now.getMinutes()
+    nowSecond = now.getSeconds()
+
+    intervalHour = parseInt(endHour) - parseInt(nowHour)
+    intervalMinute = parseInt(endMinute) - parseInt(nowMinute)
+    intervalSecond = parseInt(endSecond) - parseInt(nowSecond)
+    interval = intervalHour * 3600 + intervalMinute * 60 + intervalSecond
+    counting();
+
+  })
+
+  $('input[type="time"][val="now"]').each(function(){    
+    let d = new Date(),        
+        h = d.getHours(),
+        m = d.getMinutes();
+    if(h < 10) h = '0' + h; 
+    if(m < 10) m = '0' + m; 
+    $(this).attr({
+      'value': h + ':' + m
+    });
+  });
+});
